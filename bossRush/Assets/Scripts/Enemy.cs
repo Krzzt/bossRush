bebbb83c46d262c0a64a6d9ca1b5c6100e6bb0df
Krzzt <ferdinand.knoblauch@gmail.com>
2024-09-25 15:38:51 +0200

    3rd

--- bossRush/Assets/Scripts/Enemy.cs
+++ bossRush/Assets/Scripts/Enemy.cs
@@ -7,8 +7,17 @@ public class Enemy : MonoBehaviour
     public UnitHealth EnemyHealth = new UnitHealth(5, 5);
     public int bossID;
     public int currPhase;
-    // Start is called before the first frame update
 
+    public int healthIncrease;
+
+    public float movetimer;
+    public float timebetweenmoves;
+
+    private void Awake()
+    {
+        EnemyHealth._currentMaxHealth += healthIncrease;
+        EnemyHealth._currentHealth += healthIncrease;
+    }
     public void TakeDamage(int amount)
     {
         EnemyHealth.DamageUnit(amount);
@@ -20,23 +29,54 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         switch (bossID)
         {
             //Boss1
             case 0:
-                switch (currPhase)
+                   if (timebetweenmoves < movetimer)
                 {
-                    case 1:
-
-                        break;
-
-                    case 2:
-
-                        break;
+                    timebetweenmoves += Time.deltaTime;
                 }
+                   else
+                {
+                    BossBehaivour0();
+                    
+                }
+                   
+                
                 break;
         }
     }
+
+
+public void BossBehaivour0()
+    {
+        switch (currPhase) {
+            
+            case 1:
+                int moveToDo = Random.Range(0, 3);
+                switch (moveToDo)
+                {
+                    case 0:
+
+
+                        //move1
+                        timebetweenmoves = 0;
+                        break;
+
+                    case 1:
+                        //move2
+                        timebetweenmoves = 0;
+                        break;
+
+                    case 2:
+                        //move3
+                        timebetweenmoves = 0;
+                        break;
+                }
+              break;
+    }
+
 }
