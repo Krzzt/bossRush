012f6ee88896123386c03c2478ae292c15f1058b
Krzzt <ferdinand.knoblauch@gmail.com>
2024-09-27 14:15:53 +0200

    aaa

--- bossRush/Assets/Scripts/BossBehaivour/Boss1.cs
+++ bossRush/Assets/Scripts/BossBehaivour/Boss1.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Boss1 : MonoBehaviour
@@ -19,6 +21,8 @@ public class Boss1 : MonoBehaviour
 
     public GameObject bulletPrefab;
 
+    public GameObject empty;
+
     public int bulletFireForce;
 
     public float time;
@@ -32,7 +36,9 @@ public class Boss1 : MonoBehaviour
     private int xpostomove = 0;
     private int ypostomove = 0;
 
-    public Vector3 PlayerPos;
+    private Vector3 PlayerPos;
+
+    private GameObject circle;
     private void Awake()
     {
         PlayerObject = GameObject.FindWithTag("player");
@@ -51,26 +57,95 @@ public class Boss1 : MonoBehaviour
         move();
         if (attack1active)
         {
-            GameObject[] bulletsToFire = new GameObject[4];
-            bulletsToFire[0] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(0, 5, 0), new Quaternion(0, 0, 180, 0));
-            bulletsToFire[1] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(5, 0, 0), new Quaternion(0, 0, 90, 0));
-            bulletsToFire[2] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(-5, 0, 0), new Quaternion(0, 0, -90, 0));
-            bulletsToFire[3] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(0, -5, 0), new Quaternion(0, 0, 0, 0));
-            for (int i = 0; i < bulletsToFire.Length; i++)
+            time += Time.fixedDeltaTime;
+            if (time > 0.66f)
             {
-               // bulletsToFire[i].GetComponent<Rigidbody2D>().AddForce();
+               
+                if (amountBullets % 2 == 0)
+                {
+                    GameObject[] bulletsToFire = new GameObject[4];
+                    bulletsToFire[0] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(0, 5, 0), new Quaternion(0, 0, 180, 0));
+                    bulletsToFire[1] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(5, 0, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[2] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(-5, 0, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[3] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(0, -5, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[1].transform.Rotate(0, 0, 90);
+                    bulletsToFire[2].transform.Rotate(0, 0, -90);
+                    for (int i = 0; i < bulletsToFire.Length; i++)
+                    {
+                        bulletsToFire[i].GetComponent<Rigidbody2D>().AddForce(bulletsToFire[i].transform.up * bulletFireForce, ForceMode2D.Impulse);
+                    }
+                }
+                else if (amountBullets % 2 != 0)
+                {
+                    GameObject[] bulletsToFire = new GameObject[4];
+                    bulletsToFire[0] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(2.5f, 2.5f, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[1] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(-2.5f, 2.5f, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[2] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(-2.5f, -2.5f, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[3] = Instantiate(bulletPrefab, PlayerObject.transform.position + new Vector3(2.5f, -2.5f, 0), new Quaternion(0, 0, 0, 0));
+                    bulletsToFire[0].transform.Rotate(0, 0, 135);
+                    bulletsToFire[1].transform.Rotate(0, 0, -135);
+                    bulletsToFire[2].transform.Rotate(0, 0, -45);
+                    bulletsToFire[3].transform.Rotate(0, 0, 45);
+                    for (int i = 0; i < bulletsToFire.Length; i++)
+                    {
+                        bulletsToFire[i].GetComponent<Rigidbody2D>().AddForce(bulletsToFire[i].transform.up * bulletFireForce, ForceMode2D.Impulse);
+                    
+                    }
+                }
+
+                amountBullets++;
+                time = 0;
             }
+
+            
+            if (amountBullets >= 4)
+            {
+                amountBullets = 0;
+                time = 0;
+                attack1active = false;
+            }
+          
 
 
         }
         else if (attack2active)
         {
 
-          
+            time += Time.fixedDeltaTime;
+            PlayerPos = PlayerObject.transform.position;
+            circle.transform.position = gameObject.transform.position;
+            
+            
+            if ( Vector3.Distance(PlayerPos, gameObject.transform.position) > Vector3.Distance(PlayerPos, circle.transform.GetChild(0).transform.position))
+            {
+                gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, circle.transform.GetChild(0).transform.position, Time.fixedDeltaTime * speed * 4);
+
+            }
+            else
+            {
+                
+                gameObject.transform.position = circle.transform.position;
+                circle.transform.Rotate(0, 0, 5);
+                if (time >= 1)
+                {
+                    GameObject bulletFire = Instantiate(bulletPrefab, gameObject.transform.position, gameObject.transform.rotation);
+                    bulletFire.GetComponent<Rigidbody2D>().AddForce(bulletFire.transform.up * bulletFireForce, ForceMode2D.Impulse);
+                    amountBullets++;
+                }
+
+            }
+           
+            if (amountBullets >= 10)
+            {
+                time = 0;
+                amountBullets = 0;
+                attack2active = false;
+            }
+
         }
         else if (attack3active)
         {
-
+            attack3active = false;
             
         }
         else
@@ -105,6 +180,7 @@ public class Boss1 : MonoBehaviour
 
                             case 1:
                                 //move2
+                                circle = Instantiate(empty, PlayerPos, Quaternion.identity);
                                 attack2active = true;
                                 break;
 
@@ -135,20 +211,23 @@ public class Boss1 : MonoBehaviour
             direction.Normalize();
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-        
-        float xpos = 0;
-        float ypos = 0;
-        if (frames % Random.Range(20, 60) == 0)
+        if (!attack2active)
         {
-            xpostomove = Random.Range(-100, 101);
-            ypostomove = Random.Range(-100, 101);
+            float xpos = 0;
+            float ypos = 0;
+            if (frames % Random.Range(20, 60) == 0)
+            {
+                xpostomove = Random.Range(-100, 101);
+                ypostomove = Random.Range(-100, 101);
 
-        }
-       
+            }
+
             xpos = (float)xpostomove + this.transform.position.x;
             ypos = (float)ypostomove + this.transform.position.y;
             Vector2 moveGoal = new Vector2(xpos, ypos);
             gameObject.transform.position = Vector2.MoveTowards(this.transform.position, moveGoal, speed * Time.fixedDeltaTime);
+        }
+
     }
 
 
